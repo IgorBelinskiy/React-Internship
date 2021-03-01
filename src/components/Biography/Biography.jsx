5a8e31428ddbae2cc34bca57b756c855ba1b85d4
IgorBelinskiy <i.belinskiy92@gmail.com>
2021-03-01 11:51:04 +0200

    function

--- src/components/Biography/Biography.jsx
+++ src/components/Biography/Biography.jsx
@@ -3,6 +3,7 @@ import classes from './Biography.module.css';
 import TableBody from './TableBody/TableBody';
 
 
+
 const biographyObj = {
    state: {
       biographyData: [
@@ -14,70 +15,75 @@ const biographyObj = {
       ],
       type: true,
    },
-   addPost(data) {
+   addPost(data, state) {
       if (data.year === 0 || isNaN(data.year) || data.event === '') {
          alert('Введите корректные данные!!!')
       } else {
          let newEvent = {
-            id: this.state.biographyData.length + 1,
-            // year: postMessage.year,
-            // event: postMessage.event,
+            id: state.length + 1,
             ...data,
          }
-         this.state.biographyData.push(newEvent);
+         let newBiographyData = [...state, newEvent];
+         console.log(newBiographyData);
          console.log(this.state.biographyData);
       }
 
    },
-   deleteLastPost() {
-      this.state.biographyData.splice(-1, 1);
+   deleteLastPost(state) {
+      let newBiographyData = [...state];
+      newBiographyData.splice(-1, 1);
+      console.log(newBiographyData);
       console.log(this.state.biographyData);
    },
-   deleteSelectedItem(index) {
-      this.state.biographyData.splice(index, 1);
+   deleteSelectedItem(index, state) {
+      let newBiographyData = [...state];
+      newBiographyData.splice(index, 1);
+      console.log(newBiographyData);
       console.log(this.state.biographyData);
    },
-   toMax() {
-      this.state.biographyData.sort((a, b) => a.year - b.year);
+   toMax(state) {
+      let newBiographyData = [...state];
+      newBiographyData.sort((a, b) => a.year - b.year);
+      console.log(newBiographyData);
       console.log(this.state.biographyData);
    },
-   toMin() {
-      this.state.biographyData.sort((a, b) => b.year - a.year);
+   toMin(state) {
+      let newBiographyData = [...state];
+      newBiographyData.sort((a, b) => b.year - a.year);
+      console.log(newBiographyData);
       console.log(this.state.biographyData);
    },
-   minMax(type = this.state.type) {
-      (type === true) ? this.state.biographyData.sort((a, b) => a.year - b.year) : this.state.biographyData.sort((a, b) => b.year - a.year);
+   minMax(state, type = this.state.type) {
+      let newBiographyData = [...state];
+      (type === true) ? newBiographyData.sort((a, b) => a.year - b.year) : newBiographyData.sort((a, b) => b.year - a.year);
+      console.log(newBiographyData);
       console.log(this.state.biographyData);
       this.state.type = !this.state.type;
    },
-   rndm() {
+   rndm(state) {
+      let newBiographyData = [...state];
       let i, j, k;
-      for (i = this.state.biographyData.length - 1; i > 0; i--) {
+      for (i = newBiographyData.length - 1; i > 0; i--) {
          j = Math.floor(Math.random() * i);
-         k = this.state.biographyData[i];
-         this.state.biographyData[i] = this.state.biographyData[j];
-         this.state.biographyData[j] = k;
+         k = newBiographyData[i];
+         newBiographyData[i] = newBiographyData[j];
+         newBiographyData[j] = k;
       }
+      console.log(newBiographyData)
       console.log(this.state.biographyData);
    },
-   bubbleSort() {
-      for (let n = 0; n < this.state.biographyData.length; n++) {
-         for (let i = 0; i < this.state.biographyData.length - 1 - n; i++) {
-            if (this.state.biographyData[i].year > this.state.biographyData[i + 1].year) {
-               const buff = this.state.biographyData[i];
-               this.state.biographyData[i] = this.state.biographyData[i + 1];
-               this.state.biographyData[i + 1] = buff;
+   bubbleSort(state) {
+      let newBiographyData = [...state];
+      for (let n = 0; n < newBiographyData.length; n++) {
+         for (let i = 0; i < newBiographyData.length - 1 - n; i++) {
+            if (newBiographyData[i].year < newBiographyData[i + 1].year) {
+               const buff = newBiographyData[i];
+               newBiographyData[i] = newBiographyData[i + 1];
+               newBiographyData[i + 1] = buff;
             }
          }
       }
-      let updateBiographyData = this.state.biographyData.map((el, index) => {
-         return {
-            id: index + 1,
-            year: el.year,
-            event: el.event,
-         }
-      })
-      this.state.biographyData = updateBiographyData;
+      console.log(newBiographyData)
       console.log(this.state.biographyData);
    },
    getState() {
@@ -88,24 +94,26 @@ const biographyObj = {
 
 const Biography = () => {
 
+   let { biographyData } = biographyObj.getState();
 
-   let biographyElement = biographyObj.getState().biographyData
+   let biographyElement = biographyData
       .map((el, index) => <TableBody
          deleteSelectedItem={biographyObj.deleteSelectedItem.bind(biographyObj)}
          key={el.id}
          index={index}
          year={el.year}
-         event={el.event} />)
+         event={el.event}
+         state={biographyData} />)
 
    let newYearEl = React.createRef();
    let newEventEl = React.createRef();
 
-   const onAddItemBiography = () => {
+   const onAddItemBiography = (state) => {
       let data = {
          year: +newYearEl.current.value,
          event: newEventEl.current.value,
       };
-      biographyObj.addPost(data);
+      biographyObj.addPost(data, state);
    };
 
 
@@ -127,17 +135,17 @@ const Biography = () => {
                   <textarea ref={newEventEl} placeholder='Введите событие'></textarea>
                </div>
                <div className={classes.form_button}>
-                  <button onClick={onAddItemBiography}>Add new event</button>
-                  <button onClick={() => { biographyObj.deleteLastPost() }} className={classes.del_btn}>Delete last event in list</button>
+                  <button onClick={() => { onAddItemBiography(biographyData) }}>Add new event</button>
+                  <button onClick={() => { biographyObj.deleteLastPost(biographyData) }} className={classes.del_btn}>Delete last event in list</button>
                </div>
                <div className={classes.form_action}>
                   <div className={classes.action_title}>Сортировка данных</div>
                   <div className={classes.action_items}>
-                     <div onClick={() => { biographyObj.toMax() }} className={classes.items}>#1</div>
-                     <div onClick={() => { biographyObj.toMin() }} className={classes.items}>#2</div>
-                     <div onClick={() => { biographyObj.minMax() }} className={classes.items}>#3</div>
-                     <div onClick={() => { biographyObj.rndm() }} className={classes.items}>#4</div>
-                     <div onClick={() => { biographyObj.bubbleSort() }} className={classes.items}>#5</div>
+                     <div onClick={() => { biographyObj.toMax(biographyData) }} className={classes.items}>#1</div>
+                     <div onClick={() => { biographyObj.toMin(biographyData) }} className={classes.items}>#2</div>
+                     <div onClick={() => { biographyObj.minMax(biographyData) }} className={classes.items}>#3</div>
+                     <div onClick={() => { biographyObj.rndm(biographyData) }} className={classes.items}>#4</div>
+                     <div onClick={() => { biographyObj.bubbleSort(biographyData) }} className={classes.items}>#5</div>
                   </div>
                </div>
             </div>
--- src/components/Biography/TableBody/TableBody.jsx
+++ src/components/Biography/TableBody/TableBody.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import classes from './TableBody.module.css'
 import PropTypes from 'prop-types';
 
-const TableBody = ({ index, year, event, deleteSelectedItem }) => {
+const TableBody = ({ index, year, event, deleteSelectedItem, state }) => {
 
    return (
       <div className={classes.table_body}>
          <div className={classes.body_number}>{index + 1}</div>
          <div className={classes.body_year}>{year}</div>
          <div className={classes.body_event}>{event}</div>
-         <div onClick={() => { deleteSelectedItem(index) }} className={classes.event_del}>del</div>
+         <div onClick={() => { deleteSelectedItem(index, state) }} className={classes.event_del}>del</div>
       </div >
    )
 }
@@ -19,6 +19,7 @@ TableBody.propTypes = {
    year: PropTypes.number,
    event: PropTypes.string,
    deleteSelectedItem: PropTypes.func,
+   state: PropTypes.array
 }
 
 export default TableBody;
