103aa408f043187c2b92edb5af322d28d9a65326
IgorBelinskiy <i.belinskiy92@gmail.com>
2021-04-02 15:45:49 +0300

    state, props, life-cycle (upd)

--- src/components/API/Api.jsx
+++ src/components/API/Api.jsx
@@ -11,21 +11,25 @@ class Api extends React.Component {
    state = {
       users: [],
       isFetching: true,
+      error: false,
    }
 
    componentDidMount() {
       axios.get('https://jsonplaceholder.typicode.com/users')
          .then(response => response.data)
          .then((data) => this.setState({ users: data, isFetching: false, }))
+         .catch(() => this.setState({ error: true }))
    }
 
    render() {
 
-      const { users, isFetching } = this.state;
+      const { users, isFetching, error } = this.state;
       if (isFetching) {
          return (
             <div className={classes.api}>
                <Preloader />
+               {error && 'Ошибка...'}
+               <ButtonHome />
             </div>
          )
       }
